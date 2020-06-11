92ea1cea199dcd998a289776032251db3c7308c7
Ivan Oštrić <ivan@halcyon.hr>
2020-06-11 17:56:43 +0200

    🤦 Typo in download URL

--- Formula/sunshine.rb
+++ Formula/sunshine.rb
@@ -3,7 +3,7 @@ class Sunshine < Formula
 
   desc "☀️ when does the sun rise?"
   homepage "https://github.com/crescentrose/sunshine"
-  url "https://github.com/crescentrose/sunshine/releases/download/#{@current_version}/sunshine-mac.tar.gz"
+  url "https://github.com/crescentrose/sunshine/releases/download/v#{@current_version}/sunshine-mac.tar.gz"
   sha256 "3363cc43d71ad7fa1ba4f627627a45e8e21c0efa386a52d9833df9759cdb5cbf"
   version @current_version
 
